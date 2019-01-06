98a074bb7c33172afbc5dd971f31ce5a2aa09dc0
HappyWindManS <3066004354@qq.com>
2019-01-06 19:00:52 +0800

    GGle a

--- MR/ASPNETMVC/MusicStore01/MusicStore/Controllers/StoreController.cs
+++ MR/ASPNETMVC/MusicStore01/MusicStore/Controllers/StoreController.cs
@@ -30,27 +30,209 @@ namespace MusicStore.Controllers
         /// <summary>
         /// 点赞
         /// </summary>
-        /// <param name="id">赞或踩</param>
+        /// <param name="pid">回复id</param>
         /// <returns></returns>
         [HttpPost]
-        public ActionResult Like(Guid id)
+        public ActionResult Like(Guid pid)
         {
-            //1.判断用户是否登录
+            //初始化存储对象
+            var status = new LikeStatus()
+            {
+                Status = "",
+                Html = "",
+            };
+            //1判断用户是否登录
             if (Session["LoginUserSessionModel"] == null)
-                return Json("nologin");
+            {
+                status = new LikeStatus()
+                {
+                    Status = "noLogin",
+                    Html = "",
+                };
+                return Json(status);
+            }
             //2.判断用户是否对这条回复点过赞或踩
+            var person = _context.Persons.Find((Session["LoginUserSessionModel"] as LoginUserSessionModel).Person.ID);
+            var reply = _context.Replies.Find(pid);//当前评论
+            //是否有踩赞记录
+            var IsLike = _context.LikeReplies.SingleOrDefault(x => x.Reply.ID == reply.ID && x.Person.ID == person.ID);
 
-            //取出用户ID
-            var person = _context.Persons.Find((Session["LoginUserSessionModel"] as
-            LoginUserSessionModel).Person.ID);
-            //查询该评论信息
-            var reply = _context.Replies.Find(id);
-            var like = _context.LikeReplies.SingleOrDefault(x => x.Person.ID == person.ID && x.Reply.ID == reply.ID);
 
-            //3.保存 reply是体重Like+1或Hate+1 LikeReply添加一条记录
+            //3.保存  reply实体中like+1或hate+1  LikeReply添加一条记录
+            if (IsLike == null || IsLike.Person.ID != person.ID)
+            {
 
-            //生成HTML注入视图
-            return Json("OK");
+                reply.Like += 1;//添加赞数
+                var ok = new LikeReply()
+                {
+                    Reply = reply,
+                    IsNotLike = true,
+                    Person = person
+                };
+                _context.LikeReplies.Add(ok);
+                _context.SaveChanges();
+            }
+            //点赞失败，已经点过了
+            else
+            {
+                //赞或踩过了 判断
+                if (IsLike.IsNotLike == false)
+                {
+                    status.Status = "false";
+                    status.Html = "";
+                    return Json(status);
+                }
+                reply.Like -= 1;
+                _context.LikeReplies.Remove(IsLike);
+                _context.SaveChanges();
+            }
+
+            //根据是否有上级回复 刷新detail 或 modal
+            if (reply.ParentReply == null)
+            {
+                //显示评论、排序
+                var albumSay = _context.Replies.Where(x => x.Album.ID == reply.Album.ID && x.ParentReply == null)
+                    .OrderByDescending(x => x.CreateDateTime).ToList();
+
+                status.Status = "Parent";
+                status.Html = _GetHtml(albumSay);
+            }
+            else
+            {
+                //查询子回复
+                var pcmt = _context.Replies.Find(pid).ParentReply;//主
+                var cmts = _context.Replies.Where(x => x.ParentReply.ID == pcmt.ID).OrderByDescending(x => x.CreateDateTime).ToList();
+
+
+                status.Status = "Son";
+                status.Html = _GetHtmlModal(pcmt, cmts);
+            }
+            //生成html 注入视图
+            return Json(status);
+        }
+
+        /// <summary>
+        /// 踩
+        /// </summary>
+        /// <param name="pid">回复id</param>
+        /// <returns></returns>
+        public ActionResult Hate(Guid pid)
+        {
+            //初始化存储对象
+            var status = new LikeStatus()
+            {
+                Status = "",
+                Html = "",
+            };
+            //1判断用户是否登录
+            if (Session["LoginUserSessionModel"] == null)
+            {
+                status = new LikeStatus()
+                {
+                    Status = "noLogin",
+                    Html = "",
+                };
+                return Json(status);
+            }
+            //2.判断用户是否对这条回复点过赞或踩
+            var person = _context.Persons.Find((Session["LoginUserSessionModel"] as LoginUserSessionModel).Person.ID);
+            var reply = _context.Replies.Find(pid);//当前评论
+            //是否有踩赞记录
+            var IsLike = _context.LikeReplies.SingleOrDefault(x => x.Reply.ID == reply.ID && x.Person.ID == person.ID);
+
+
+            //3.保存  reply实体中like+1或hate+1  LikeReply添加一条记录
+            if (IsLike == null || IsLike.Person.ID != person.ID)
+            {
+
+                reply.Hate += 1;//添加赞数
+                var ok = new LikeReply()
+                {
+                    Reply = reply,
+                    IsNotLike = false,
+                    Person = person
+                };
+                _context.LikeReplies.Add(ok);
+                _context.SaveChanges();
+            }
+            //点赞失败，已经点过了
+            else
+            {
+                //赞或踩过了 判断
+                if (IsLike.IsNotLike == true)
+                {
+                    status.Status = "false";
+                    status.Html = "";
+                    return Json(status);
+                }
+                reply.Hate -= 1;
+                _context.LikeReplies.Remove(IsLike);
+                _context.SaveChanges();
+            }
+
+            //刷新detail 或 modal
+            if (reply.ParentReply == null)
+            {
+                //显示评论、排序
+                var albumSay = _context.Replies.Where(x => x.Album.ID == reply.Album.ID && x.ParentReply == null)
+                    .OrderByDescending(x => x.CreateDateTime).ToList();
+
+                status.Status = "Parent";
+                status.Html = _GetHtml(albumSay);
+            }
+            else
+            {
+                //查询子回复
+                var pcmt = _context.Replies.Find(pid).ParentReply;//主
+                var cmts = _context.Replies.Where(x => x.ParentReply.ID == pcmt.ID).OrderByDescending(x => x.CreateDateTime == x.CreateDateTime).ToList();
+
+
+                status.Status = "Son";
+                status.Html = _GetHtmlModal(pcmt, cmts);
+            }
+            //生成html 注入视图
+            return Json(status);
+        }
+
+        /// <summary>
+        /// 生成子回复html文本 模态框
+        /// </summary>
+        /// <param name=""></param>
+        /// <returns></returns>
+        public string _GetHtmlModal(Reply pcmt, List<Reply> cmts)
+        {
+            var htmlString = "";
+
+            htmlString += "<div class=\"modal-header\">";
+            htmlString += "<button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-hidden=\"true\">×</button>";
+            htmlString += "<h4 class=\"modal-title\" id=\"myModalLabel\">";
+            htmlString += "<em>楼主&nbsp&nbsp</em>" + pcmt.Person.Name + "  发表于" + pcmt.CreateDateTime.ToString("yyyy年MM月dd日 hh点mm分ss秒") + ":<br/>" + pcmt.Content;
+            htmlString += " </h4> </div>";
+
+            htmlString += "<div class=\"modal-body\">";
+            //子回复
+            htmlString += "<ul class='media-list' style='margin-left:20px;'>";
+        
+            foreach (var item in cmts)
+            {
+                htmlString += "<li class='media'>";
+                htmlString += "<div class='media-left'>";
+                htmlString += "<img class='media-object' id='ReplyImg' src='" + item.Person.Avarda + "' alt=\"头像\">";
+                htmlString += "</div>";
+                htmlString += "<div class='media-body' id='Content-" + item.ID + "'>";
+                htmlString += "<h5 class='media-heading'><em>" + item.Person.Name + "</em>&nbsp;&nbsp;回复  " + item.ParentReply.Person.Name + ": " + item.ParentReply.Content + " </h5> ";
+                htmlString += item.Content;
+                htmlString += "</div>";
+                htmlString += "<h6 style =\"margin-bottom:5px; border-bottom:1px solid #c6e6e8\">" + item.CreateDateTime.ToString("yyyy年MM月dd日 hh时mm分ss秒") +
+                              "<a href='#div-editor' style='margin-left:20px;' class='reply' onclick=\"javascript:GetQuote('" + item.ParentReply.ID + "','" + item.ID + "');\">回复</a>" +
+                              "<a href='#' class='reply' style='margin-left:20px;'   onclick=\"javascript:Like('" + item.ID + "');\"><i class='glyphicon glyphicon-thumbs-up'></i>(" + item.Like + ")</a>" +
+                              "<a href='#' class='reply' style='margin-left:20px;'   onclick=\"javascript:Hate('" + item.ID + "');\"><i class='glyphicon glyphicon-thumbs-down'></i>(" + item.Hate + ")</a></h6>";
+                htmlString += "</li>";
+            }
+            htmlString += "</ul>";
+            htmlString += "</div><div class=\"modal-footer\"></div>";
+
+            return htmlString;
         }
 
         #region HTML注入
@@ -183,6 +365,12 @@ namespace MusicStore.Controllers
         {
             var genres = _context.Genres.OrderBy(x => x.Name).ToList();
             return View(genres);
+        }
+
+        public class LikeStatus
+        {
+            public string Status { get; set; }
+            public string Html { get; set; }
         }
     }
 }
